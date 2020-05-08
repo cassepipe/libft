733d0350b969fdac40c20114b01ece2477593808
cassepipe <cassepipe@ymail.com>
2020-05-08 11:54:08 +0200

    libft.h complete

--- libft.h
+++ libft.h
@@ -18,6 +18,8 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n);
 
 size_t	ft_strlen(const char *s);
 
+char	*ft_strdup(const char *s);
+
 char	*ft_strchr(const char *s, int c);
 
 char	*ft_strrchr(const char *s, int c);
@@ -26,13 +28,21 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n);
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+
+char	*ft_strnstr(const char *big, const char *little, size_t len);
+
 int	ft_atoi(const char *nptr);
 
 int	ft_isalnum(int c);
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
 
 int	ft_toupper(int c);
 int	ft_tolower(int c);
+
+void	*ft_calloc(size_t nmemb, size_t size);
 
 #endif
