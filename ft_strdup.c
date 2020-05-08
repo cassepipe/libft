3cf52bdbf0c0b1c3aa6a5884767164087712dd65
cassepipe <cassepipe@ymail.com>
2020-05-08 18:21:37 +0200

    Fixed strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -10,6 +10,22 @@ char *ft_strdup(const char *s)
 	if (!duplicate)
 		return (NULL);
 	while (s[i])
+	{
 		duplicate[i] = s[i];
+		i++;
+	}
 	return duplicate;	
 }
+
+
+//char *ft_strdup(const char *s)
+//{
+//	size_t len;
+//	char *ptr;
+//
+//	len = ft_strlen(s);
+//	ptr = malloc(len + 1);
+//	if (!ptr) 
+//		return NULL;
+//	return ft_memcpy(ptr, s, len + 1);
+//}
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,11 +1,28 @@
 #include "libft.h"
 
+#include <string.h>
+/*
+int ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t i;
+
+	i = 0;
+	if (!n) return 0;
+	while (s1[i] && s2[i] && i < n && s1[i] == s2[i])
+	{
+			i++;
+	}
+	return s1[1] - s2[i];
+}
+*/
+//*
 int ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t i;
 	int diff;
 
 	i = 0;
+	diff = 0;
 	while (i < n)
 	{
 		if ((diff = s1[i] - s2[i]))
@@ -16,20 +33,28 @@ int ft_strncmp(const char *s1, const char *s2, size_t n)
 	}
 	return diff;
 }
-
+//*/
+/*
 #include <string.h>
 #include <stdio.h>
 
-/*
  int main(int argc, char** argv)
 {
 	char* a1 = argv[1];
 	char* a2 = argv[2];
 	
+
+	printf("string.h output :\n");
+	printf("Result : %d\n",strncmp(a1, a2, 0));
+	printf("Result : %d\n",strncmp(a1, a2, 1));
+	printf("Result : %d\n",strncmp(a1, a2, 2));
+	printf("Result : %d\n",strncmp(a1, a2, 4));
+	
+	printf("My output :\n");
+	printf("Result : %d\n",ft_strncmp(a1, a2, 0));
+	printf("Result : %d\n",ft_strncmp(a1, a2, 1));
 	printf("Result : %d\n",ft_strncmp(a1, a2, 2));
 	printf("Result : %d\n",ft_strncmp(a1, a2, 4));
-	
 	return 0;
 }
-
 */
