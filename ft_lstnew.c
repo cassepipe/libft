06ec9f1ab7bfe4a65abca4c9e92be0ed816c2271
cassepipe <cassepipe@ymail.com>
2020-05-25 11:28:07 +0200

    Protected lstnew malloc

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: tpouget <cassepipe@ymail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/14 18:22:50 by tpouget           #+#    #+#             */
-/*   Updated: 2020/05/23 17:24:30 by tpouget          ###   ########.fr       */
+/*   Updated: 2020/05/25 11:27:40 by tpouget          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ t_list	*ft_lstnew(void *content)
 	t_list *new_node;
 
 	new_node = malloc(sizeof(t_list));
+	if (!new_node)
+		return (NULL);
 	new_node->content = content;
 	new_node->next = NULL;
 	return (new_node);
