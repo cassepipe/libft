810c60bd9e0a92136f253fad7120e7a9bfbdd7a9
cassepipe <cassepipe@ymail.com>
2020-05-26 14:16:07 +0200

    fucking hell

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,12 +6,27 @@
 /*   By: tpouget <cassepipe@ymail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/14 17:00:07 by tpouget           #+#    #+#             */
-/*   Updated: 2020/05/26 11:03:28 by tpouget          ###   ########.fr       */
+/*   Updated: 2020/05/26 11:09:21 by tpouget          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static void *ft_memalloc(size_t size)
+{
+	void	*data;
+
+	data = malloc(size);
+	if (data)
+		ft_bzero(data, size);
+	return (data);
+}
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	return (ft_memalloc(count * size));
+}
+/*
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*data;
@@ -21,3 +36,4 @@ void	*ft_calloc(size_t count, size_t size)
 		ft_bzero(data, size);
 	return (data);
 }
+*/
