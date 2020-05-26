3e18c1d5ca08805730fd2a6da97e0d7f18c4fd33
cassepipe <cassepipe@ymail.com>
2020-05-26 11:03:44 +0200

    calloc verite

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,23 +6,18 @@
 /*   By: tpouget <cassepipe@ymail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/14 17:00:07 by tpouget           #+#    #+#             */
-/*   Updated: 2020/05/26 10:48:21 by tpouget          ###   ########.fr       */
+/*   Updated: 2020/05/26 11:03:28 by tpouget          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void *ft_memalloc(size_t size)
+void	*ft_calloc(size_t count, size_t size)
 {
 	void	*data;
 
-	data = malloc(size);
+	data = malloc(count * size);
 	if (data)
 		ft_bzero(data, size);
 	return (data);
-}
-
-void	*ft_calloc(size_t count, size_t size)
-{
-	return (ft_memalloc(count * size));
 }
