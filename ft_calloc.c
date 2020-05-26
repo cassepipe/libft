848fc7a46bab18bcbb0c28239e759e47e53ba999
cassepipe <cassepipe@ymail.com>
2020-05-26 15:17:39 +0200

    Fixed size pb in calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,12 +6,13 @@
 /*   By: tpouget <cassepipe@ymail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/14 17:00:07 by tpouget           #+#    #+#             */
-/*   Updated: 2020/05/26 11:09:21 by tpouget          ###   ########.fr       */
+/*   Updated: 2020/05/26 15:17:11 by tpouget          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*
 static void *ft_memalloc(size_t size)
 {
 	void	*data;
@@ -26,14 +27,13 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	return (ft_memalloc(count * size));
 }
-/*
+*/
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*data;
 
 	data = malloc(count * size);
 	if (data)
-		ft_bzero(data, size);
+		ft_bzero(data, count * size);
 	return (data);
 }
-*/
