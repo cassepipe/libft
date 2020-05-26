66c97debcd58955389014d20f47db5aee30316b0
cassepipe <cassepipe@ymail.com>
2020-05-26 10:27:08 +0200

    Changed behaviour in calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,18 +6,28 @@
 /*   By: tpouget <cassepipe@ymail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/14 17:00:07 by tpouget           #+#    #+#             */
-/*   Updated: 2020/05/14 17:00:13 by tpouget          ###   ########.fr       */
+/*   Updated: 2020/05/26 10:25:20 by tpouget          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+/*
+static void *ft_memalloc(size_t size)
+{
+	void	*data;
 
+	data = malloc(size);
+	if (data)
+		ft_bzero(data, size);
+	return (data);
+}
+*/
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*ret;
 
-	if (!(ret = malloc(nmemb * size)))
-		return (NULL);
-	ft_bzero(ret, size);
+	ret = malloc(nmemb * size);
+	if (ret)
+		ft_bzero(ret, size);
 	return (ret);
 }
